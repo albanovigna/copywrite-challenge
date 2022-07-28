a7b92608e1f99c2a76b1906b3ae7c226cf2b1dff
albanovigna <anvignaduzzi@gmail.com>
2022-07-28 10:53:44 -0300

    cleaning code

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -5,7 +5,6 @@ export const GET_TEXT = "GET_TEXT";
 export const getText = (payload) => {
   return async (dispatch) => {
     const result = await axios.get(`/iecho?text=${payload}`);
-    console.log(result.data);
     dispatch({ type: GET_TEXT, info: result.data });
   };
 };
